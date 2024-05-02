9fdf04e3da335731250f346bfd7cde92465f792b
Max Wiseman <maxwise70@hotmail.co.uk>
2024-05-02 14:24:05 +0000

    text adjustment and color adjustment

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function flash() {
     const text = document.getElementById('flashText');
-    text.style.color = (text.style.color === 'blue') ? 'green' : 'orange';
+    text.style.color = (text.style.color === 'red') ? 'green' : 'red';
 }
 
 setInterval(flash, 1000);
