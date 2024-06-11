c6fcc0032d4fddd8368ecf6c146e1cd75e911dbd
Max Wiseman <maxwise70@hotmail.co.uk>
2024-06-11 18:11:22 +0000

    Ran all code through W2C now with no errors, let my project down missing these small touches

--- script.js
+++
@@ -1,8 +0,0 @@
-function flash() {
-    const text = document.getElementById('flashText');
-    text.style.color = (text.style.color === 'red') ? 'green' : 'red';
-}
-
-setInterval(flash, 1000);
-
-/* code reasurched from https://www.bing.com/search?pglt=41&q=how+to+get+text+to+flash+different+colors+html+css&cvid=db48e436c54c4c8e9c163fea72130182&gs_lcrp=EgZjaHJvbWUyBggAEEUYOdIBCDgwODZqMGoxqAIIsAIB&FORM=ANNTA1&PC=LCTS&showconv=1*/
